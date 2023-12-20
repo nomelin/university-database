22a50f2c86ab203b3b13ab0210ab224701866c77
nomelin <2023686595@qq.com>
2023-12-20 14:52:00 +0800

    1

--- src/nomelin/DatabaseController.java
+++ src/nomelin/DatabaseController.java
@@ -7,5 +7,68 @@ import nomelin.entity.Teacher;
 import java.util.List;
 
 public class DatabaseController implements DatabaseControlInterface{
+    @Override
+    public void connectToDatabase() {
 
+    }
+
+    @Override
+    public void disconnectFromDatabase() {
+
+    }
+
+    @Override
+    public void insertStudent(String studentID, String name, String gender, String birthday, String classID) {
+
+    }
+
+    @Override
+    public Student queryStudentByID(String studentID) {
+        return null;
+    }
+
+    @Override
+    public List<Student> queryStudentByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Student> queryStudentByMajorID(String majorID) {
+        return null;
+    }
+
+    @Override
+    public List<Student> queryStudentByMajorName(String majorName) {
+        return null;
+    }
+
+    @Override
+    public void insertScore(String studentID, String courseName, String isMakeUp, double score) {
+
+    }
+
+    @Override
+    public List<Course> queryCoursesByStudentID(String studentID) {
+        return null;
+    }
+
+    @Override
+    public double queryAverageScoreForCompulsoryCourses(String studentID) {
+        return 0;
+    }
+
+    @Override
+    public double queryAverageScoreForAllCourses(String studentID) {
+        return 0;
+    }
+
+    @Override
+    public List<Teacher> queryTeachersForStudent(String studentID) {
+        return null;
+    }
+
+    @Override
+    public List<Student> queryStudentsNearDismissal(int creditThreshold) {
+        return null;
+    }
 }
